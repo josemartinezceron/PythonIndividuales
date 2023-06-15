72d8fdf2798e61c58d210349d22235564720d4e9
josemartinezceron <jose.f.martinezc@gmail.com>
2023-06-14 21:04:26 -0400

    subida general hasta el 6.3, faltando algunos detalles

--- Individual 6.3/sitiomodelos/appmodelos/views.py
+++ Individual 6.3/sitiomodelos/appmodelos/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 
-# Create your views here.
+def usuarios(request):
+    users = User.objects.all()
+    context = {
+        'usuarios':users,
+
+    }
+    return render(request, 'usuarios.html',context=context)
+
+def home(request):
+    return render(request, 'index.html')
