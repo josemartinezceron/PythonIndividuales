683185a39bc3c7bc2b62f95ea78c3fab6c8560c9
josemartinezceron <jose.f.martinezc@gmail.com>
2023-06-30 20:09:36 -0400

    SUBIDOS LOS ULTIMOS CAMBIOS

--- Individual 7.1/sitiounidad7/appunidad7/views.py
+++ Individual 7.1/sitiounidad7/appunidad7/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from .forms import TaskForm
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html' )
 
 def task(request):
-    return render(request, 'task.html', {'form':TaskForm})
+    return render(request, 'task.html')
+
+def signout(requets):
+    logout(request)
+    return
